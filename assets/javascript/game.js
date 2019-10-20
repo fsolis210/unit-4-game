6e44501ac39dccdb1286f7bb80eece6283238d1e
naglaswangas <fernandosolisjr@gmail.com>
2019-10-20 03:51:09 -0500

    Added more js and jquery code but cant seem to make the buttons trigger.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,4 +5,89 @@ $(document).ready(function(){
 
 $("#randomNumber").text(rando);
 })
-// Create random n umbers for each jewel
+// Create variables for gems (with random numbers), totals, wins and losses
+var gemYellow= Math.floor(Math.random()*15)
+var gemRed= Math.floor(Math.random()*15)
+var gemGreen= Math.floor(Math.random()*15)
+var gemBlue= Math.floor(Math.random()*15)
+
+var playerTotal=0;
+var wins=0;
+var losses=0;
+
+$("#numWins").text(wins);
+$("numLosses").text(losses);
+
+
+function reset(){
+    rando=Math.floor(Math.random()*25+86-7);
+    
+    $("randomNumber").text(rando);
+    gemYellow= Math.floor(Math.random()*15+1);
+    gemRed= Math.floor(Math.random()*15);
+    gemGreen= Math.floor(Math.random()*15);
+    gemBlue= Math.floor(Math.random()*15);
+    playerTotal=0;
+    $("playersTotal").text(playerTotal);
+
+    }
+
+// add wins
+function winner(){
+    wins++;
+    $("#numWins").text(wins);
+    reset()
+}
+
+// add losses
+function loser(){
+    losses++;
+    $("#numLosses").text(losses);
+    reset()
+}
+
+// add click commands to gems
+$("#yellow").on("click", function(){
+    playerTotal = playerTotal + gemYellow;
+    $("playersTotal").text(playerTotal);
+        if (playerTotal == rando)
+        winner();
+        else if (playerTotal > rando)
+        loser();
+})
+
+$("#red").on("click", function(){
+    userTotal = userTotal + gemRed;
+    $("playersTotal").text(userTotal);
+        if (userTotal == rando){
+        winner();
+        }
+        else if (userTotal > rando){
+        loser();
+        }
+})
+
+$("#green").on("click", function(){
+    userTotal = userTotal + gemGreen;
+    $("playersTotal").text(userTotal);
+        if (userTotal == rando){
+        winner();
+        }
+        else if (userTotal > rando){
+        loser();
+        }
+})
+
+$("blue").on("click", function(){
+    userTotal = userTotal + gemBlue;
+    $("playersTotal").text(userTotal);
+        if (userTotal == rando){
+        winner();
+        }
+        else if (userTotal > rando){
+        loser();
+        }
+})
+
+
+
