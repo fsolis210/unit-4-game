d23614147013dc725374b9ec36d159eb303660f5
naglaswangas <fernandosolisjr@gmail.com>
2019-10-24 18:58:29 -0500

    Fix players score to show score and update it when button is clicked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,31 +4,31 @@ $(document).ready(function(){
 
 
 $("#randomNumber").text(rando);
-})
+
 // Create variables for gems (with random numbers), totals, wins and losses
-var gemYellow= Math.floor(Math.random()*15)
-var gemRed= Math.floor(Math.random()*15)
-var gemGreen= Math.floor(Math.random()*15)
-var gemBlue= Math.floor(Math.random()*15)
+var gemYellow= Math.floor(Math.random()*15+1)
+var gemRed= Math.floor(Math.random()*15+1)
+var gemGreen= Math.floor(Math.random()*15+1)
+var gemBlue= Math.floor(Math.random()*15+1)
 
 var playerTotal=0;
 var wins=0;
 var losses=0;
 
 $("#numWins").text(wins);
-$("numLosses").text(losses);
+$("#numLosses").text(losses);
 
 
 function reset(){
     rando=Math.floor(Math.random()*25+86-7);
     
-    $("randomNumber").text(rando);
+    $("#randomNumber").text(rando);
     gemYellow= Math.floor(Math.random()*15+1);
     gemRed= Math.floor(Math.random()*15);
     gemGreen= Math.floor(Math.random()*15);
     gemBlue= Math.floor(Math.random()*15);
     playerTotal=0;
-    $("playersTotal").text(playerTotal);
+    $("#playersTotal").text(playerTotal);
 
     }
 
@@ -49,45 +49,45 @@ function loser(){
 // add click commands to gems
 $("#yellow").on("click", function(){
     playerTotal = playerTotal + gemYellow;
-    $("playersTotal").text(playerTotal);
+    $("#playersTotal").text(playerTotal);
         if (playerTotal == rando)
         winner();
-        else if (playerTotal > rando)
+        if (playerTotal > rando)
         loser();
 })
 
 $("#red").on("click", function(){
-    userTotal = userTotal + gemRed;
-    $("playersTotal").text(userTotal);
-        if (userTotal == rando){
+    playerTotal = playerTotal + gemRed;
+    $("#playersTotal").text(playerTotal);
+        if (playerTotal == rando){
         winner();
         }
-        else if (userTotal > rando){
+        if (playerTotal > rando){
         loser();
         }
 })
 
 $("#green").on("click", function(){
-    userTotal = userTotal + gemGreen;
-    $("playersTotal").text(userTotal);
-        if (userTotal == rando){
+    playerTotal = playerTotal + gemGreen;
+    $("#playersTotal").text(playerTotal);
+        if (playerTotal == rando){
         winner();
         }
-        else if (userTotal > rando){
+        if (playerTotal > rando){
         loser();
         }
 })
 
-$("blue").on("click", function(){
-    userTotal = userTotal + gemBlue;
-    $("playersTotal").text(userTotal);
-        if (userTotal == rando){
+$("#blue").on("click", function(){
+    playerTotal = playerTotal + gemBlue;
+    $("#playersTotal").text(playerTotal);
+        if (playerTotal == rando){
         winner();
         }
-        else if (userTotal > rando){
+        if (playerTotal > rando){
         loser();
         }
 })
-
+})
 
 
